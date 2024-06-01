c76b1a0519461e963923ef919af79b6e310bc0db
mike <m.nagler995@gmail.com>
2024-06-01 18:49:10 -0400

    Implement String() function for nodes

--- source/monkey/ast/ast.go
+++ source/monkey/ast/ast.go
@@ -1,9 +1,14 @@
 package ast
 
-import "github.com/naglepuff/monkey/token"
+import (
+	"bytes"
+
+	"github.com/naglepuff/monkey/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -28,6 +33,14 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+	return out.String()
+}
+
 type LetStatement struct {
 	Token token.Token // the token.LET token (can this be a "class constant"?)
 	Name  *Identifier
@@ -39,9 +52,25 @@ func (ls *LetStatement) TokenLiteral() string {
 	return ls.Token.Literal
 }
 
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
 type Identifier struct {
 	Token token.Token // the token.IDENT token
 	Value string
+}
+
+func (i *Identifier) String() string {
+	return i.Value
 }
 
 func (i *Identifier) expressionNode() {}
@@ -50,11 +79,33 @@ func (i *Identifier) TokenLiteral() string {
 }
 
 type ReturnStatement struct {
-	Token token.Token // the token.RETURN token
-	Value Expression
+	Token       token.Token // the token.RETURN token
+	ReturnValue Expression
 }
 
 func (rs *ReturnStatement) statementNode() {}
 func (rs *ReturnStatement) TokenLiteral() string {
 	return rs.Token.Literal
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
